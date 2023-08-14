c674d5c68bd79355ef2558d783357b353662a2dd
Matthias Kurz <m.kurz@irregular.at>
2023-08-14 02:00:36 +0200

    More testing (also staging jars)

--- main-project/app/controllers/HomeController.scala
+++ main-project/app/controllers/HomeController.scala
@@ -2,12 +2,20 @@ package controllers
 
 import javax.inject._
 import play.api._
+import play.api.db.Database
 import play.api.mvc._
 
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(val controllerComponents: ControllerComponents, val database: Database) extends BaseController {
 
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok("hello world")
+    val value = database.withConnection(connection => {
+      val rs = connection
+        .prepareStatement("select * from table1")
+        .executeQuery()
+      rs.next()
+      rs.getString(1)
+    })
+    Ok(value)
   }
 }
--- main-project/build.sbt
+++ main-project/build.sbt
@@ -17,3 +17,14 @@ libraryDependencies += evolutions
 libraryDependencies += "com.h2database" % "h2" % "2.2.220"
 
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "6.0.0-M6" % Test
+
+PlayKeys.includeDocumentationInBinary := false
+
+//In Windows when "input path is too long" or similar message
+//scriptClasspath := Seq("*")
+
+//PlayKeys.externalizeResources := false
+
+//Universal / mappings ++= (baseDirectory.value / "asdf" ** "*").get.map {
+//  (f: File) => f -> f.relativeTo(baseDirectory.value).get.toString
+//}
